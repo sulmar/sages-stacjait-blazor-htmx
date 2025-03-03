38abe638e2b2ba0374117fad1e4e0605add3e7d2
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-03-03 19:08:23 +0100

    Dodanie hx-get

--- src/HtmxMinimal/Program.cs
+++ src/HtmxMinimal/Program.cs
@@ -4,6 +4,13 @@ var app = builder.Build();
 // Udostępnie plików statycznych z katalogu wwwroot
 app.UseStaticFiles();
 
+app.MapGet("/time", () =>
+{
+    var now = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+    
+    return $"<p>Aktualny czas: {now}</p>";
+});
+
 // Ustawienie index.html jako strony głównej
 app.MapFallbackToFile("index.html");
 
